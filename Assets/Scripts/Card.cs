776784ccc68842e49c8488d51dfd5f272cbbac73
Jarin-Gobi <kimhyerin0220@gmail.com>
2025-08-14 00:31:16 +0900

    뽑기 완료
    시작, 게임오버, 플레이방법, 딜러, 미니 카드, 숫자 구현해야함

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -3,9 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Card : MonoBehaviour
 {
+    // 카드 보여주기
+    public Image cardDisplayImage;
+
+    // 카드
     public List<List<int>> Gachalist = new List<List<int>>();
     public List<int> Spade = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
     public List<int> Heart = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
@@ -22,10 +27,68 @@ public class Card : MonoBehaviour
 
     public void Gacha()
     {
+        // 모양 선택
         int rand = Random.Range(0, Gachalist.Count);
-        int rand2 = Random.Range(0, Gachalist[rand].Count);
-        print(rand);
-        print(Gachalist[rand][rand2]);
-        Gachalist[rand].RemoveAt(rand2);
+
+        // 카드 개수 확인
+        if (Gachalist[rand].Count > 0)
+        {
+            int CardNum = 0;
+            // 카드 뽑기
+            int rand2 = Random.Range(0, Gachalist[rand].Count);
+
+            string shape = "";
+       
+            if (Gachalist[rand] == Heart) {
+                CardNum = Gachalist[rand][rand2] - 1;
+                shape = "1.2 Poker cards_" + CardNum;
+            }
+            else if (Gachalist[rand] == Diamond) {
+                CardNum = 14 + Gachalist[rand][rand2];
+                shape = "1.2 Poker cards_" + CardNum;
+            }
+            else if (Gachalist[rand] == Spade) {
+                CardNum = 27 + Gachalist[rand][rand2];
+                shape = "1.2 Poker cards_" + CardNum;
+            }
+            else if (Gachalist[rand] == Clova) {
+                CardNum = 40 + Gachalist[rand][rand2];
+                shape = "1.2 Poker cards_" + CardNum;
+            }
+
+
+            Sprite cardSprite = Resources.Load<Sprite>(shape);
+
+            // 카드 표시
+            if (cardSprite != null)
+            {
+                cardDisplayImage.sprite = cardSprite;
+
+            }
+            else
+            {
+                Debug.LogError(shape + " 이름의 카드 이미지X");
+            }
+
+            Debug.Log(Gachalist[rand][rand2]);
+
+            // 제거
+            Gachalist[rand].RemoveAt(rand2);
+        }
+        else
+        {
+            Gachalist.RemoveAt(rand);
+
+            // 남아있나?
+            if (Gachalist.Count > 0)
+            {
+                Gacha();
+            }
+            else if(Gachalist.Count == 0)
+            {
+                Debug.Log("카드X");
+                return;
+            }
+        }
     }
 }
---
+++ Assets/Scripts/Minicards.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minicards : MonoBehaviour
+{
+    public int order = 0;
+
+    private void Awake()
+    {
+        
+    }
+
+    public void Order()
+    {
+
+    }
+}
